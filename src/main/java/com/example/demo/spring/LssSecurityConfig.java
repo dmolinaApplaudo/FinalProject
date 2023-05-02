b1a34b2a209728801cdf702bbf176ffce4c11780
dmolina <dmolina@applaudostudios.dev>
2023-05-02 11:12:59 -0600

    Immplemented Configuration Classes

--- src/main/java/com/example/demo/spring/LssSecurityConfig.java
+++ src/main/java/com/example/demo/spring/LssSecurityConfig.java
@@ -1,4 +1,44 @@
 package com.example.demo.spring;
 
+import com.example.demo.keycloak.KeycloakLogoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@Configuration
+@EnableWebSecurity
 public class LssSecurityConfig {
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    LssSecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
+    @Bean
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/customers*")
+                .hasRole("USER")
+                .anyRequest()
+                .permitAll();
+        http.oauth2Login()
+                .and()
+                .logout()
+                .addLogoutHandler(keycloakLogoutHandler)
+                .logoutSuccessUrl("/");
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
+        return http.build();
+    }
 }
