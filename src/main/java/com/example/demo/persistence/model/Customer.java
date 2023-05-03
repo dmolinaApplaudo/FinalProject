7cbeb4c46b4d0094d6b742461852d9c7f6e07d20
dmolina <dmolina@applaudostudios.dev>
2023-05-03 16:47:39 -0600

    Changed the Hash Equal and Customer Table to accomodate for new value

--- src/main/java/com/example/demo/persistence/model/Customer.java
+++ src/main/java/com/example/demo/persistence/model/Customer.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Objects;
@@ -18,16 +20,21 @@ public class Customer {
     @NotNull
     private long customerId;
 
+    @NotNull(message = "Customers Name can not be Null")
+    @NotBlank(message = "Customers Name can not be left Blank")
+    @Size(max = 50)
+    private String name;
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Customer customer = (Customer) o;
-        return customerId == customer.customerId;
+        return customerId == customer.customerId && Objects.equals(name, customer.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(customerId);
+        return Objects.hash(customerId, name);
     }
 }
--- src/main/resources/db/migration/V1__createCustomerTable.sql
+++ src/main/resources/db/migration/V1__createCustomerTable.sql
@@ -1,4 +1,5 @@
 CREATE TABLE customer (
   customer_id BIGINT NOT NULL,
+   name VARCHAR(50) NOT NULL,
    CONSTRAINT pk_customer PRIMARY KEY (customer_id)
 );
