2c021a761637d21a9092d05b30fa7aa6a60170c4
dmolina <dmolina@applaudostudios.dev>
2023-05-03 15:49:10 -0600

    Finished Setting up the actuators

--- src/main/java/com/example/demo/spring/LssSecurityConfig.java
+++ src/main/java/com/example/demo/spring/LssSecurityConfig.java
@@ -1,8 +1,9 @@
 package com.example.demo.spring;
 
-import com.example.demo.keycloak.KeycloakLogoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -14,11 +15,9 @@ import org.springframework.security.web.authentication.session.SessionAuthentica
 @Configuration
 @EnableWebSecurity
 public class LssSecurityConfig {
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
 
-    LssSecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-        this.keycloakLogoutHandler = keycloakLogoutHandler;
-    }
+    private final String defaultUser = "SCOPE_openid";
+
 
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
@@ -27,18 +26,27 @@ public class LssSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/customers*")
-                .hasRole("USER")
-                .anyRequest()
-                .permitAll();
+
+        http.csrf().disable();
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/actuator/health")
+                .permitAll()
+                .requestMatchers("/actuator").hasAnyAuthority(defaultUser)
+                .requestMatchers("/actuator/metrics").hasAnyAuthority(defaultUser)
+                .requestMatchers("/actuator/loggers").hasAnyAuthority(defaultUser);
+
         http.oauth2Login()
                 .and()
                 .logout()
-                .addLogoutHandler(keycloakLogoutHandler)
                 .logoutSuccessUrl("/");
         http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .build();
     }
 }
