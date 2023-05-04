f781bf25c415aa54950aca7b036c4aff2fda7bb2
dmolina <dmolina@applaudostudios.dev>
2023-05-04 08:35:05 -0600

    Created Initial Checkout Controller

--- src/main/java/com/example/demo/web/controller/StartCheckoutController.java
+++ src/main/java/com/example/demo/web/controller/StartCheckoutController.java
@@ -1,4 +1,25 @@
 package com.example.demo.web.controller;
 
+import com.example.demo.persistence.model.Checkout;
+import com.example.demo.service.StartCheckoutService;
+import com.example.demo.web.controller.requestbody.InitialCheckoutBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/checkout")
 public class StartCheckoutController {
+
+    private StartCheckoutService startCheckoutService;
+    @PostMapping(value = "/start")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Checkout startCheckout(@RequestBody InitialCheckoutBody initialCheckoutBody){
+        return startCheckoutService.startCheckout(
+                initialCheckoutBody.getCustomerId(),
+                initialCheckoutBody.getProductId(),
+                initialCheckoutBody.getProductQuantity()
+        );
+    }
 }
