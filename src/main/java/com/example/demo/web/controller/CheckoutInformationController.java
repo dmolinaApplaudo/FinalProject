cb4e7a90cff1cdea7a64b4ae82c682ea8b244af3
dmolina <dmolina@applaudostudios.dev>
2023-05-04 08:24:08 -0600

    Created Controller to check if the Checkout was saved successfully

--- src/main/java/com/example/demo/web/controller/CheckoutInformationController.java
+++ src/main/java/com/example/demo/web/controller/CheckoutInformationController.java
@@ -1,11 +1,26 @@
 package com.example.demo.web.controller;
 
+import com.example.demo.persistence.model.Checkout;
+import com.example.demo.service.impl.CheckOutInfo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping
+@RequestMapping(value = "/checkout")
 public class CheckoutInformationController {
+
+    private CheckOutInfo checkOutInfo;
+
+    @GetMapping
+    @ResponseBody
+    List<Checkout> findAll(){
+        return checkOutInfo.findAll();
+    }
+
 }
